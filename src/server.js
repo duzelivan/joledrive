e0fe0177a9f0740a9200b4a6fba0801d95beaa40
duzelivan <duzelivan94@gmail.com>
2026-05-25 18:42:32 +0200

    Update server.js

--- src/server.js
+++ src/server.js
@@ -60,6 +60,7 @@ app.use('/api/notifications', require('./routes/notifications'));
 app.use('/api/settings', require('./routes/settings'));
 app.use('/api/vehicle-assignments', require('./routes/vehicleAssignments'));
 app.use('/api/mileage', require('./routes/mileage'));
+app.use('/api/share', require('./routes/share'));
 
 // ============================================
 // CRON - Automatske dnevne obavijesti
